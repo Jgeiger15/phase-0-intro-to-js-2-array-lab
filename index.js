059ddd34df5354c7c7dc365b1c08257e325bd090
Jakob Geiger <jakobgeiger15@gmail.com>
2021-10-19 20:21:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+  
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = cats.slice();
+    newCats.push("Broom");
+    return newCats;
+}
+
+function prependCat() {
+    const newCats = cats.slice();
+    newCats.unshift("Arnold");
+    return newCats;
+}
+function removeLastCat () {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat () {
+    const newCats = cats.slice(1);
+    return newCats;
+}
